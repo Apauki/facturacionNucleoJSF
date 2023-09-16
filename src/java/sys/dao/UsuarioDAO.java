dac8b9cdceb258193284bacc74c2d4107fef2913
Apauki <111468497+Apauki@users.noreply.github.com>
2023-09-16 09:30:54 -0500

    first commit

---
+++ src/java/sys/dao/UsuarioDAO.java
@@ -0,0 +1,11 @@
+
+package sys.dao;
+
+import sys.modelo.Usuario;
+
+public interface UsuarioDAO {
+    
+    public Usuario search(Usuario usuario);
+    public Usuario login(Usuario usuario);
+            
+}
